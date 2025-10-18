dc637f7136b1732cf78525b4c1a2bc25a821b6db
McGlock <mclaughlinr2@gmail.com>
2025-10-18 11:37:16 -0700

    fiddling

--- run_vsearch.sh
+++ run_vsearch.sh
@@ -95,9 +95,9 @@ while [[ $# -gt 0 ]]; do
 done
 
 # Define directories
-INPUT_DIR="/home/ryan/SeqData/SeqData/UBC/LMP_priority1/fastq_methods"
+INPUT_DIR="/home/ryan/SeqData/SeqData/UBC/LMP_priority1/fastq_combined"
 REFDB_DIR="/home/ryan/SeqData/SeqData/UBC/LMP_priority1/ref_db"
-OUTPUT_DIR="/home/ryan/SeqData/SeqData/UBC/LMP_priority1/spark_methods_output_tester"
+OUTPUT_DIR="/home/ryan/SeqData/SeqData/UBC/LMP_priority1/spark_refactor_output_older"
 QC_DIR="${OUTPUT_DIR}/fastp"
 MERGED_DIR="${OUTPUT_DIR}/merged"
 FILTERED_DIR="${OUTPUT_DIR}/filtered"
